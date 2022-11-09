118dd30e17396469209a71c046574985d98f8060
Andy Bowskill <andybowskill@gmail.com>
2022-11-09 11:03:35 +0000

    Update the tests

--- device/ciscodevice_test.go
+++ device/ciscodevice_test.go
@@ -12,17 +12,34 @@ func TestBackupCiscoCmds(t *testing.T) {
 	var b = new(bytes.Buffer)
 	var in io.Writer = b
 
+	enable := "enable"
 	password := "12345"
+	termLength := "terminal length 0"
 	showrunCmd := "show run brief"
+	noTermLength := "terminal no length 0"
+	exit := "exit"
 
 	backupCiscoCmds(in, password)
 
 	got := b.String()
 
+	if !strings.Contains(got, enable) {
+		t.Errorf("Expected Cisco device contains '%s'", enable)
+	}
 	if !strings.Contains(got, password) {
 		t.Errorf("Expected device password of '%s'", password)
+	}
+	if !strings.Contains(got, termLength) {
+		t.Errorf("Expected Cisco device contains '%s'", termLength)
 	}
 	if !strings.Contains(got, showrunCmd) {
 		t.Errorf("Expected show run command of '%s'", showrunCmd)
 	}
+	if !strings.Contains(got, noTermLength) {
+		t.Errorf("Expected Cisco device contains '%s'", noTermLength)
+	}
+	if !strings.Contains(got, exit) {
+		t.Errorf("Expected Cisco device contains '%s'", exit)
+	}
+
 }
--- device/networkdevice_test.go
+++ device/networkdevice_test.go
@@ -8,28 +8,22 @@ import (
 
 func TestBackupNetworkValidArgs(t *testing.T) {
 
-	var nds NetworkDevices
-
 	bytes := []byte("networkdevices:\r\n  - type: cisco\r\n    username: andybowskill\r\n    password: cisco\r\n    ipv4: '192.168.48.2:22'\r\n")
-	yaml.Unmarshal(bytes, &nds)
 
-	backupToCisco := func(nd NetworkDevice) {}
-
-	backupNetwork(&nds, backupToCisco)
-
-	got := len(nds.NetworkDevices)
-	want := 1
-
-	if got != want {
-		t.Errorf("Expected NetworkDevice element of %d isn't the same actual of %d", got, want)
-	}
+	backupNetworkArgs(t, bytes, 1)
 }
 
 func TestBackupNetworkInvalidArgs(t *testing.T) {
 
+	bytes := []byte("networkdevices:")
+
+	backupNetworkArgs(t, bytes, 0)
+}
+
+func backupNetworkArgs(t *testing.T, bytes []byte, want int) {
+
 	var nds NetworkDevices
 
-	bytes := []byte("networkdevices:")
 	yaml.Unmarshal(bytes, &nds)
 
 	backupToCisco := func(nd NetworkDevice) {}
@@ -37,9 +31,9 @@ func TestBackupNetworkInvalidArgs(t *testing.T) {
 	backupNetwork(&nds, backupToCisco)
 
 	got := len(nds.NetworkDevices)
-	want := 0
 
 	if got != want {
 		t.Errorf("Expected NetworkDevice element of %d isn't the same actual of %d", got, want)
 	}
+
 }
